ff2b070215e82dd99e01c86e4a1807e5c3876595
Danylo Pasko <danil.pasco@gmail.com>
2025-12-03 15:52:54 +0200

    initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack
+                initialRouteName="homeStack"
+                screenOptions={{ headerShown: false }}
+            />
+        </GestureHandlerRootView>
+    );
+}
--- src/app/homeStack/Home.tsx
+++ src/app/homeStack/Home.tsx
@@ -1,6 +1,9 @@
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Home() {
+    const router = useRouter();
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
